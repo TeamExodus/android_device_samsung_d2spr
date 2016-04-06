83ea5d5766a9973c21f7d3ca3be2b9b1b57adc65
Raja M <raja1612.m@gmail.com>
2016-04-06 09:41:15 +0530

    Bring up d2spr Samsung SIII sprint with EXODUS-6.0

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/d2spr/d2spr-vendor.mk)
+$(call inherit-product, vendor/samsung/d2spr/d2spr-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2spr/overlay
